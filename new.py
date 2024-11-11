6466171befada734214d0690618c1e96e17f04e2
unknown <aroragarima129@gmail.com>
2024-11-11 21:52:59 +0530

    First commit

---
+++ new.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Skincare Recommender")
+st.header("Skincare Recommendation System")
+
+st.markdown("# Page 1 ❄️")
+st.sidebar.markdown("# Page 1 ❄️")
